8a9c2ac5f5773f50c7353ed2e1ab053cc6656ad7
persianprime <persianprime@gmail.com>
2023-11-23 00:42:01 +0330

    Export

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,58 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.Button btnAutoCapture;
-            System.Windows.Forms.Button btnCaptureSingle;
-            System.Windows.Forms.Button btnAbortCapturing;
             System.Windows.Forms.Button btnExtract;
             System.Windows.Forms.Button btnSaveImage;
             System.Windows.Forms.Button btnStartCapturing;
             System.Windows.Forms.Button btnInit;
             System.Windows.Forms.Button btnClear;
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.lbScannerList = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pbImageFrame = new System.Windows.Forms.PictureBox();
             this.tbxMessage = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            btnAutoCapture = new System.Windows.Forms.Button();
-            btnCaptureSingle = new System.Windows.Forms.Button();
-            btnAbortCapturing = new System.Windows.Forms.Button();
             btnExtract = new System.Windows.Forms.Button();
             btnSaveImage = new System.Windows.Forms.Button();
             btnStartCapturing = new System.Windows.Forms.Button();
             btnInit = new System.Windows.Forms.Button();
             btnClear = new System.Windows.Forms.Button();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbImageFrame)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnAutoCapture
-            // 
-            btnAutoCapture.Location = new System.Drawing.Point(213, 16);
-            btnAutoCapture.Name = "btnAutoCapture";
-            btnAutoCapture.Size = new System.Drawing.Size(96, 24);
-            btnAutoCapture.TabIndex = 18;
-            btnAutoCapture.Text = "Auto Capture";
-            btnAutoCapture.UseVisualStyleBackColor = true;
-            // 
-            // btnCaptureSingle
-            // 
-            btnCaptureSingle.Location = new System.Drawing.Point(8, 46);
-            btnCaptureSingle.Name = "btnCaptureSingle";
-            btnCaptureSingle.Size = new System.Drawing.Size(96, 24);
-            btnCaptureSingle.TabIndex = 14;
-            btnCaptureSingle.Text = "Capture Single";
-            btnCaptureSingle.UseVisualStyleBackColor = true;
-            // 
-            // btnAbortCapturing
-            // 
-            btnAbortCapturing.Location = new System.Drawing.Point(213, 46);
-            btnAbortCapturing.Name = "btnAbortCapturing";
-            btnAbortCapturing.Size = new System.Drawing.Size(96, 24);
-            btnAbortCapturing.TabIndex = 13;
-            btnAbortCapturing.Text = "Abort";
-            btnAbortCapturing.UseVisualStyleBackColor = true;
             // 
             // btnExtract
             // 
@@ -89,6 +54,7 @@
             btnExtract.TabIndex = 11;
             btnExtract.Text = "ذخیره در بانک اطلاعاتی";
             btnExtract.UseVisualStyleBackColor = true;
+            btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
             // 
             // btnSaveImage
             // 
@@ -98,21 +64,23 @@
             btnSaveImage.TabIndex = 17;
             btnSaveImage.Text = "دریافت تصویر";
             btnSaveImage.UseVisualStyleBackColor = true;
+            btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
             // 
             // btnStartCapturing
             // 
             btnStartCapturing.Location = new System.Drawing.Point(144, 271);
             btnStartCapturing.Name = "btnStartCapturing";
-            btnStartCapturing.Size = new System.Drawing.Size(96, 24);
+            btnStartCapturing.Size = new System.Drawing.Size(96, 54);
             btnStartCapturing.TabIndex = 10;
             btnStartCapturing.Text = "شروع اسکن";
             btnStartCapturing.UseVisualStyleBackColor = true;
+            btnStartCapturing.Click += new System.EventHandler(this.btnStartCapturing_Click);
             // 
             // btnInit
             // 
-            btnInit.Location = new System.Drawing.Point(478, 12);
+            btnInit.Location = new System.Drawing.Point(257, 16);
             btnInit.Name = "btnInit";
-            btnInit.Size = new System.Drawing.Size(112, 24);
+            btnInit.Size = new System.Drawing.Size(334, 31);
             btnInit.TabIndex = 9;
             btnInit.Text = "آماده سازی اسکنر";
             btnInit.UseVisualStyleBackColor = true;
@@ -120,41 +88,30 @@
             // 
             // btnClear
             // 
-            btnClear.Location = new System.Drawing.Point(275, 271);
+            btnClear.Location = new System.Drawing.Point(257, 171);
             btnClear.Name = "btnClear";
             btnClear.Size = new System.Drawing.Size(44, 93);
             btnClear.TabIndex = 15;
             btnClear.Text = "خالی کردن";
             btnClear.UseVisualStyleBackColor = true;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(btnAutoCapture);
-            this.groupBox2.Controls.Add(btnCaptureSingle);
-            this.groupBox2.Controls.Add(btnAbortCapturing);
-            this.groupBox2.Location = new System.Drawing.Point(275, 164);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(315, 80);
-            this.groupBox2.TabIndex = 13;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Use Scanner";
+            btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // lbScannerList
             // 
             this.lbScannerList.FormattingEnabled = true;
-            this.lbScannerList.Location = new System.Drawing.Point(275, 93);
+            this.lbScannerList.Location = new System.Drawing.Point(257, 90);
             this.lbScannerList.Name = "lbScannerList";
-            this.lbScannerList.Size = new System.Drawing.Size(315, 43);
+            this.lbScannerList.Size = new System.Drawing.Size(333, 43);
             this.lbScannerList.TabIndex = 12;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(518, 67);
+            this.label1.Location = new System.Drawing.Point(511, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(83, 13);
             this.label1.TabIndex = 11;
-            this.label1.Text = "انتخاب اسکنر:";
+            this.label1.Text = "لیست اسکنرها:";
             // 
             // pbImageFrame
             // 
@@ -169,18 +126,18 @@
             // 
             // tbxMessage
             // 
-            this.tbxMessage.Location = new System.Drawing.Point(324, 271);
+            this.tbxMessage.Location = new System.Drawing.Point(307, 171);
             this.tbxMessage.Multiline = true;
             this.tbxMessage.Name = "tbxMessage";
             this.tbxMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbxMessage.Size = new System.Drawing.Size(266, 93);
+            this.tbxMessage.Size = new System.Drawing.Size(283, 93);
             this.tbxMessage.TabIndex = 14;
             this.tbxMessage.WordWrap = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(542, 251);
+            this.label2.Location = new System.Drawing.Point(543, 151);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 16;
@@ -190,13 +147,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 376);
+            this.ClientSize = new System.Drawing.Size(603, 336);
             this.Controls.Add(this.label2);
             this.Controls.Add(btnClear);
             this.Controls.Add(this.tbxMessage);
             this.Controls.Add(btnSaveImage);
             this.Controls.Add(btnExtract);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.lbScannerList);
             this.Controls.Add(btnStartCapturing);
             this.Controls.Add(this.label1);
@@ -207,10 +163,10 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "اسکن اثر انگشت";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbImageFrame)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -218,8 +174,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ListBox lbScannerList;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pbImageFrame;
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Suprema;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace FingerPrint
 {
@@ -16,6 +17,31 @@ namespace FingerPrint
     {
         UFScannerManager m_ScannerManager;
         string m_strError;
+        byte[][] m_template1;
+        int[] m_template_size1;
+        byte[][] m_template2;
+        int[] m_template_size2;
+        int m_template_num;
+        string[] m_UserID;
+        int m_quality;
+        int m_nType;
+        //
+        byte[] m_pUFTemplateBuf;
+        int[] m_pUFTemplateBufSize;
+        byte[] m_pISOTemplateBuf;
+        int[] m_pISOTemplateBufSize;
+        //
+        const int MAX_TEMPLATE_SIZE = 1024;
+        const int MAX_TEMPLATE_NUM = 50;
+
+        const int MAX_USERID_SIZE = 10;
+        const int MAX_TEMPLATE_INPUT_NUM = 4;
+        const int MAX_TEMPLATE_OUTPUT_NUM = 2;
+
+        const int FINGERDATA_COL_SERIAL = 0;
+        const int FINGERDATA_COL_USERID = 1;
+        const int FINGERDATA_COL_TEMPLATE1 = 2;
+        const int FINGERDATA_COL_TEMPLATE2 = 3;
         public Form1()
         {
             InitializeComponent();
@@ -28,6 +54,9 @@ namespace FingerPrint
 
         private void btnInit_Click(object sender, EventArgs e)
         {
+            //=========================================================================//
+            // Initilize scanners
+            //=========================================================================//
             UFS_STATUS ufs_res;
             int nScannerNumber;
 
@@ -44,9 +73,77 @@ namespace FingerPrint
                 Cursor.Current = this.Cursor;
                 return;
             }
+
+            m_ScannerManager.ScannerEvent += new UFS_SCANNER_PROC(ScannerEvent);
+
+            nScannerNumber = m_ScannerManager.Scanners.Count;
+            tbxMessage.AppendText("UFScanner GetScannerNumber: " + nScannerNumber + "\r\n");
+
             UpdateScannerList();
 
-          Cursor.Current = this.Cursor;
+            
+
+
+            Cursor.Current = this.Cursor;
+        }
+        private delegate void _UpdatePictureBox(PictureBox pbox, Bitmap image);
+
+        public void UpdatePictureBox(PictureBox pbox, Image image)
+        {
+            if (pbox.InvokeRequired)
+            {
+                _UpdatePictureBox del = new _UpdatePictureBox(UpdatePictureBox);
+                // Call the function in the correct thread
+                BeginInvoke(del, new object[] { pbox, image });
+            }
+            else
+            {
+                // We are in the correct thread, so assign the image
+                pbox.Image = image;
+                //System.Threading.Thread.Sleep(100);
+            }
+        }
+        public int CaptureEvent(object sender, UFScannerCaptureEventArgs e)
+        {
+            int lfdScore;
+            // We cannot use pbImageFrame.Image directly from the different thread,
+            // so we use UpdatePictureBox() to update PictureBox indirectly
+            UFScanner temp = (UFScanner)sender;
+            if (temp.DetectFake > 0 && isPreview == false)
+            {
+                temp.GetLFDScore(e.ImageFrame, out lfdScore);
+                if (this.tbxMessage.InvokeRequired)
+                {
+                    this.tbxMessage.Invoke(new MethodInvoker(delegate ()
+                    {
+                        tbxMessage.AppendText("SDK LFD score : " + lfdScore + "\r\n");
+                    }));
+                }
+                else
+                {
+                    this.tbxMessage.AppendText("SDK LFD score : " + lfdScore + "\r\n");
+                }
+            }
+
+            UpdatePictureBox(pbImageFrame, e.ImageFrame);
+            //MessageBox.Show("Enroll UI is not supported."+ sender.ToString());
+            if (e.FingerOn)
+            {
+                m_LFDImg = e.ImageFrame;
+            }
+
+            return 1;
+        }
+        public void ScannerEvent(object sender, UFScannerManagerScannerEventArgs e)
+        {
+            if (e.SensorOn)
+            {
+                UpdateScannerList();
+            }
+            else
+            {
+                UpdateScannerList();
+            }
         }
         private void UpdateScannerList()
         {
@@ -79,6 +176,143 @@ namespace FingerPrint
             }
 
 
+        }
+
+        public Bitmap m_LFDImg;
+        public bool isPreview;
+        private void btnStartCapturing_Click(object sender, EventArgs e)
+        {
+            UFScanner Scanner;
+            UFS_STATUS ufs_res;
+            if (!GetGetCurrentScanner(out Scanner))
+            {
+                return;
+            }
+            isPreview = true;
+            Scanner.CaptureEvent += new UFS_CAPTURE_PROC(CaptureEvent);
+            ufs_res = Scanner.StartCapturing();
+            if (ufs_res == UFS_STATUS.OK)
+            {
+                tbxMessage.AppendText("UFScanner StartCapturing: OK\r\n");
+            }
+            else
+            {
+                UFScanner.GetErrorString(ufs_res, out m_strError);
+                tbxMessage.AppendText("UFScanner StartCapturing: " + m_strError + "\r\n");
+            }
+        }
+        private bool GetGetCurrentScanner(out UFScanner Scanner)
+        {
+            Scanner = m_ScannerManager.Scanners[lbScannerList.SelectedIndex];
+            if (Scanner != null)
+            {
+                return true;
+            }
+            else
+            {
+                tbxMessage.AppendText("Selected Scanner is not connected\r\n");
+                return false;
+            }
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            UFScanner Scanner;
+            UFS_STATUS ufs_res;
+            if (!GetGetCurrentScanner(out Scanner))
+            {
+                return;
+            }
+
+            Scanner.TemplateSize = MAX_TEMPLATE_SIZE;
+            Scanner.DetectCore = false;
+
+            byte[] Template = new byte[MAX_TEMPLATE_SIZE];
+            int TemplateSize;
+            int EnrollQuality;
+
+            Cursor.Current = Cursors.WaitCursor;
+            ufs_res = Scanner.ExtractEx(MAX_TEMPLATE_SIZE, Template, out TemplateSize, out EnrollQuality);
+            Cursor.Current = this.Cursor;
+
+            if (ufs_res == UFS_STATUS.OK)
+            {
+                tbxMessage.AppendText("UFScanner ExtractEx:OK(Q:{0},Len\r\n");
+                tbxMessage.AppendText(String.Format("Template Size({0}), Quailty({1})\r\n", TemplateSize, EnrollQuality));
+            }
+            else
+            {
+                UFScanner.GetErrorString(ufs_res, out m_strError);
+                tbxMessage.AppendText("UFScanner ExtractEx: " + m_strError + "\r\n");
+            }
+            DrawCapturedImage(Scanner);
+        }
+        private void DrawCapturedImage(UFScanner Scanner)
+        {
+            Graphics g = pbImageFrame.CreateGraphics();
+            Rectangle rect = new Rectangle(0, 0, pbImageFrame.Width, pbImageFrame.Height);
+            try
+            {
+                // 1. DrawCaptureImageBuffer
+                Scanner.DrawCaptureImageBuffer(g, rect, false);
+
+                // 2. GetCaptureImageBuffer
+                //Bitmap bitmap;
+                //int Resolution;
+                //Scanner.GetCaptureImageBuffer(out bitmap, out Resolution);
+                //pbImageFrame.Image = bitmap;
+            }
+            finally
+            {
+                g.Dispose();
+            }
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            UFScanner Scanner;
+            UFS_STATUS ufs_res;
+
+            int nFilterIdx = 0;
+
+            if (!GetGetCurrentScanner(out Scanner))
+            {
+                return;
+            }
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Bitmap files (*.bmp)|*.bmp";
+            dlg.DefaultExt = "bmp";
+            if (dlg.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            else
+            {
+                nFilterIdx = dlg.FilterIndex;
+            }
+
+            if (nFilterIdx == 1)
+            {
+                ufs_res = Scanner.SaveCaptureImageBufferToBMP(dlg.FileName);
+                if (ufs_res == UFS_STATUS.OK)
+                {
+                    tbxMessage.AppendText("UFScanner Image Buffer is saved to " + dlg.FileName + "\r\n");
+                }
+            }
+            else if (nFilterIdx == 2)
+            {
+                ufs_res = Scanner.SaveCaptureImageBufferToWSQ(dlg.FileName, (float)2.25);
+                if (ufs_res == UFS_STATUS.OK)
+                {
+                    tbxMessage.AppendText("UFScanner Image Buffer is saved to " + dlg.FileName + "\r\n");
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbxMessage.Clear();
         }
     }
 }
